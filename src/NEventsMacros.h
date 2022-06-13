0d3395c1c5857f6177877cb3bacdfd1c4adcf850
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-12 21:21:26 -0400

    [ADD] EVENT_LAMBA

--- src/NEventsMacros.h
+++ src/NEventsMacros.h
@@ -1,5 +1,5 @@
-
-#define EVENT_FUNCTION(name, args_name) void name(EventArgs *args_name)
-#define EVENT_FUNCTION_PTR(name) void (*name)(EventArgs *)
 #define FUNCTION_PTR(name) void (*name)(void)
+#define EVENT_FUNCTION(name, args_name) void name(EventArgs *args_name)
 #define ESR(name, args_name, defenition) EVENT_FUNCTION(name, args_name) { defenition }
+#define EVENT_LAMBDA(defenition) [](EventArgs *eventArgs) defenition
+#define EVENT_FUNCTION_PTR(name) void (*name)(EventArgs *)
