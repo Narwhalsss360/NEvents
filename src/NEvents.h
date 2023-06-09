9f020c95ba609419add4232fb48e8b05d34e5ce7
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-06-08 21:12:07 -0400

    Transition to new standard

--- src/NEvents.h
+++ src/NEvents.h
@@ -1,40 +1,40 @@
 #ifndef NEvents_h
 #define NEvents_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-    #include "Arduino.h"
-#else
-    #include "WProgram.h"
-#endif
+#include <Callables.h>
 
-#include <NDefs.h>
-#include "EventArgs.h"
-#include "Callback.h"
-#include "NEventsMacros.h"
-
-#define NEVENTS_MAJOR_VERSION 1
-#define NEVENTS_MINOR_VERSION 0
-#define NEVENTS_PATCH_VERSION 1
-
-struct EventHandler
+template <class Owner>
+class Event
 {
-    Callback *callback;
-    EventHandler();
-    virtual void addCallback(EVENT_FUNCTION_PTR());
-    virtual void invoke(EventArgs *);
-    virtual void operator+=(EVENT_FUNCTION_PTR());
+public:
+	Event(VoidCallableAny* callable = nullptr)
+		: callable(callable)
+	{
+	}
+
+	void addHandler(VoidCallableAny* callable)
+	{
+		this->callable = callable;
+	}
+
+	void operator+=(VoidCallableAny& callable)
+	{
+		addHandler(&callable);
+	}
+
+private:
+	void invoke(any_ptr pointer = nullptr)
+	{
+		callable->invoke(pointer);
+	}
+
+	void operator()(any_ptr pointer = nullptr)
+	{
+		invoke(pointer);
+	}
+
+	VoidCallableAny* callable;
+
+	friend Owner;
 };
-
-struct MultiFunctionEventHandler : public EventHandler
-{
-    uint8_t callbackCount;
-    MultiFunctionEventHandler();
-    void addCallback(EVENT_FUNCTION_PTR()) override;
-    void invoke(EventArgs *) override;
-    virtual void operator+=(EVENT_FUNCTION_PTR());
-};
-
-typedef EventHandler event;
-typedef MultiFunctionEventHandler multi_event;
-
 #endif
--- src/NEventsMacros.h
+++
@@ -1,5 +0,0 @@
-#define FUNCTION_PTR(name) void (*name)(void)
-#define EVENT_FUNCTION(name, args_name) void name(EventArgs *args_name)
-#define ESR(name, args_name, defenition) EVENT_FUNCTION(name, args_name) { defenition }
-#define EVENT_LAMBDA(defenition) [](EventArgs *eventArgs) defenition
-#define EVENT_FUNCTION_PTR(name) void (*name)(EventArgs *)
